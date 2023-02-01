9df42399b8ec725544cd8b88516a2cd2ef771fb9
Abby <abby.e.smith.85@gmail.com>
2023-02-01 14:51:13 -0700

    Done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function(){
+        alert("I was clicked!");
+    });
 }
+
+// const input = document.getElementById('button');
+// input.addEventListener('click', function () {
+//     alert("I was clicked!");
+// });
+
+
+// function clickAlert(){
+//     alert("I was clicked, with a new style!");
+
+
